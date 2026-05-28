dd7c8c1ca7474f355648389dcf5ab5d7400e27dd
Yasmin Ferrari Dias <158753555+YasminFerrariDias@users.noreply.github.com>
2026-05-28 09:25:10 -0300

    Delete 2aula directory

--- 2aula/atv5.py
+++
@@ -1,7 +0,0 @@
-# Agora faça o contrário. Você fornece a temperatura em graus Fahrenheit, seu Script converte para Celsius e exibe na tela.
-
-fahrenheit = float(input("Informe a temperatura: "))
-
-celsius = (fahrenheit - 32)/1.8
- 
-print("A temperatura em Celsius: %.2f" % (celsius))
