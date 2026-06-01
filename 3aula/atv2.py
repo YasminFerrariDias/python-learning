4a09edc910eae0a58223158891e2c8fdd7dc582a
Yasmin Ferrari Dias <158753555+YasminFerrariDias@users.noreply.github.com>
2026-06-01 15:38:33 -0300

    Delete 3aula directory

--- 3aula/atv2.py
+++
@@ -1,9 +0,0 @@
-# Você está desenvolvendo um Script em linguagem Python para calcular a área de um hexágono regular com base no raio fornecido pelo usuário. Um hexágono regular tem seis lados de igual comprimento e seis ângulos internos de 120 graus. Assim, para determinar a área desse hexágono, basta determinar a área de um dos triângulos e, em seguida, multiplicar o resultado por 6.
-
-import math
-
-raio = float(input("Informe o raio do hexágono regular (em metros): "))
-
-area = (3 * math.sqrt(3) * raio**2) / 2
-
-print("A área do hexágono é de %.2f" % (area))
